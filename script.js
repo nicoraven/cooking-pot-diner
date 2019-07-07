89e2e5df7f346d131acdc3891b760f444022ef00
khyreerusydi <nicoraventbh@gmail.com>
2019-07-07 17:59:11 +0800

    added recipes and pantry

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("loading script");
+
+// recipe list
+
+// salad: veggies, veggies, veggies
+// hot dog bun: bun, meat, sauce
+// meatballs spaghetti: pasta, meat, sauce
+// hamburger: bun, bun, meat, veggies
+// cheeseburger: bun, bun, meat, veggies, cheese
+// chocolate milkshake: milk, milk, chocolate
+// chocolate ice cream: ice, chocolate, milk
+
+const pantry = ["veggies", "bun", "meat", "sauce", "pasta", "cheese", "milk", "chocolate", "ice"];
